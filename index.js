213fbfa1a695841a6bdd505ace6c574ef9e1ec5f
Daniel Hott <daniel_hott@outlook.com>
2022-04-05 17:36:42 -0300

    Adicionando comentarios

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-function Ampulheta(tamanho) {
-  if (tamanho >= 20) {
-    for (let line = 0; line < tamanho; line += 1) {
+function Ampulheta(size) {
+
+// Se o tamanho for menor que 20 irá retornar uma mensagem de erro
+  if (size >= 20) {
+    for (let line = 0; line < size; line += 1) {
       process.stdout.write("#");
       for (let blank = 0; blank < line; blank += 1) {
         process.stdout.write(" ");
       }
-      for (let star = 0; star < (tamanho - line) * 2 - 1; star += 1) {
+      for (let star = 0; star < (size - line) * 2 - 1; star += 1) {
         process.stdout.write("#");
       }
       console.log();
     }
 
-    for (let line = 2; line <= tamanho; line += 1) {
+    for (let line = 2; line <= size; line += 1) {
       process.stdout.write("#");
-      for (let blank = 1; blank <= tamanho - line; blank += 1) {
+      for (let blank = 1; blank <= size - line; blank += 1) {
         process.stdout.write(" ");
       }
       for (let star = 0; star < 2 * line - 1; star += 1) {
@@ -21,9 +23,9 @@ function Ampulheta(tamanho) {
       }
       console.log();
     }
-  } else return console.log("Tamanho deve ser maior ou igual a 20");
+  } else return console.log("size deve ser maior ou igual a 20");
 }
 
-//Insira o número do tamanho da sua ampulheta na função
+//Insira o número do size da sua ampulheta na função
 
 Ampulheta(20);
